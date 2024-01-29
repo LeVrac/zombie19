1f091cd26b35b079dbf95576e734ab698e5c65be
LeVrac <39700660+LeVrac@users.noreply.github.com>
2024-01-29 16:55:00 +0100

    Update zombie-19.js

--- zombie-19.js
+++ zombie-19.js
@@ -16,7 +16,7 @@ class Gens {
 
     propagation() {
 
-        var listVariant = [2,5,7,11];
+        var listVariant = [2,3,5,7,11];
 
         var statubelow = 1;
 
@@ -31,11 +31,13 @@ class Gens {
         }
 
         this.down.forEach(element => {
+            
             element.infectedStatus *= infectA(this, element);
             
             element.infectedStatus *= infect32Down(this, element);
-            
             var infected = element.propagation();
+            
+        console.log("remonté : " + infected);
             listVariant.forEach(variant => {
                 if (infected % variant == 0 && statubelow % variant != 0) {
                     statubelow *= variant;
@@ -43,7 +45,7 @@ class Gens {
             })
             
         });
-
+        console.log(this.name + " " +statubelow);
         this.infectedStatus *= infectB(statubelow, this);
 
         this.infectedStatus *= infect32Up(statubelow, this);
@@ -51,15 +53,28 @@ class Gens {
         this.infectedStatus *= infectUltime(statubelow, this);
         
 
-        if (this.vaccines == true && this.infectedStatus % 2 == 1) {
+        if (this.vaccines == true && this.infectedStatus % 2 == 0) {
             this.infectedStatus /2;
         }
-        if (this.vaccines == true && this.infectedStatus % 5 == 1) {
+        if (this.vaccines == true && this.infectedStatus % 5 == 0) {
             this.infectedStatus /5;
         }
+        if (statubelow % 11 == 0 && !this.vaccines && this.infectedStatus % 11 == 1) {
+            return this.livingStatus ? this.infectedStatus * 11 : 1;
+        }
         return this.livingStatus ? this.infectedStatus : 1;
+        
     }
 
+    verifInfo() {
+
+        console.log(this.name + " " + this.infectedStatus + " " + this.vaccines+ " " +this.livingStatus);
+        this.down.forEach(element => {
+            element.verifInfo();
+            
+        });
+
+    }
     vaccinationA1() {
 
 
@@ -116,7 +131,7 @@ function infectB(below, element) {
 }
 
 function infect32Up(below, element) {
-    if (below % 5 == 0 && thelementis.infectedStatus % 5 != 0 && element.age >= 32 && element.vaccines == false && element.livingStatus == true) {
+    if (below % 5 == 0 && element.infectedStatus % 5 != 0 && element.age >= 32 && element.vaccines == false && element.livingStatus == true) {
         return 5;
     }
     return 1;
@@ -124,8 +139,9 @@ function infect32Up(below, element) {
 
 function infect32Down(parent, element) {
     if (parent.infectedStatus % 5 == 0 && element.infectedStatus % 5 != 0 && element.age >= 32 && this.vaccines == false && this.livingStatus == true) {
-        element.infectedStatus *= 5;
+        return 5;
     }
+    return 1;
 }
 
 function infectUltime(below, element) {
@@ -142,15 +158,16 @@ function infectUltime(below, element) {
 // zombie C = 7
 // zombie Ultime = 11
 
-let root = new Gens("Jean-Racine", 50, 1, 1);
-let node1 = new Gens("Jean-Pierre", 30, 2, 1);
-let node2 = new Gens("Jean-Michel", 40, 1, 1);
-let node3 = new Gens("Jean-Luc", 20, 1, "vivant", 1);
-let node11 = new Gens("4Jean-Luc", 20, 1, "vivant", 1);
-let node12 = new Gens("5Jean-Luc", 20, 5, "vivant", 1);
-let node21 = new Gens("6Jean-Luc", 20, 11, "vivant", 1);
-let node22 = new Gens("7Jean-Luc", 20, 7, "vivant", 1);
-let node221 = new Gens("8Jean-Luc", 20, 3, "vivant", 1);
+let root = new Gens("root", 50, 1, false);
+let node1 = new Gens("node1", 30, 1, false);
+let node2 = new Gens("node2", 40, 1, false);
+let node3 = new Gens("node3", 20, 1, false);
+let node11 = new Gens("node11", 20, 1, false);
+let node12 = new Gens("node12", 20, 5, false);
+let node21 = new Gens("node21", 20, 11, false);
+let node22 = new Gens("node22", 20, 7,  false);
+let node221 = new Gens("node221", 20, 3, false);
+
 
 root.addDown(node1);
 root.addDown(node2);
@@ -161,8 +178,17 @@ node2.addDown(node21);
 node2.addDown(node22);
 node21.addDown(node221);
 
-
 root.propagation();
+
+root.verifInfo();
+
+//root.verifInfo();
 root.vaccinationA1();
+
+//root.verifInfo();
 root.vaccinationB1();
+
+//root.verifInfo();
 root.vaccinationUltime();
+
+//root.verifInfo();
